56fa463435afd5a7c7f4009545f364160e5d2022
Daniel Ribeiro Jungstedt <danieljungs@oc2071024515.ibm.com>
2019-07-14 20:18:02 -0300

    Created remoteMethod auth

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Usuario) {
-
+    Usuario.auth = function(req, cb) {
+        
+        Usuario.login(req, 'user', function(err, token) {
+            if (err) {
+              cb('Erro, Cadastro não encontrado', { //render view named 'response.ejs'
+                title: 'Login failed',
+                content: err,
+                redirectTo: '/',
+                redirectToLinkText: 'Try again'
+              });
+              return;
+            }
+            cb(null,{ //login user and render 'home' view
+                username: req.username,
+                accessToken: token.id
+            });
+        });
+    };
+      Usuario.remoteMethod('auth', {
+        accepts: [{
+            arg: 'login', type: 'object'
+        }],
+        returns: {arg: 'success', type: 'string'},
+        http: {path:'/auth', verb: 'post'}
+    });
 };
