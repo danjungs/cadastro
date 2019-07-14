0ef03128963841d1a6c2321275ba15ce25c8cf7a
Daniel Ribeiro Jungstedt <danieljungs@oc2071024515.ibm.com>
2019-07-14 20:21:26 -0300

    remove some logs

--- server/boot/script.js
+++ server/boot/script.js
@@ -25,7 +25,6 @@ module.exports = function(app){
             principalId: users.id
             },(err, principal) =>{
               if (err) throw err;
-              console.log('Created principal:', principal);
             });
         })
 
